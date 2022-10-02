a17f322e9f1840ca9725c1993ab74dd393e1c77b
Vipin Patel <112804686+Vipin-patelME@users.noreply.github.com>
2022-10-02 12:37:15 +0530

    Update index.js

--- src/components/EventItem/index.js
+++ src/components/EventItem/index.js
@@ -2,19 +2,23 @@
 import './index.css'
 
 const EventItem = props => {
-  const {eachEvents} = props
-  const {imageUrl, name, location} = eachEvents
+  const {eachEvents, changedRegistationStatus} = props
+  const {imageUrl, name, location, registrationStatus} = eachEvents
 
-  const onRegistration = () => {}
+  const onRegistration = () => {
+    changedRegistationStatus(registrationStatus)
+  }
   return (
     <li className="single-list">
-      <img
-        src={imageUrl}
-        alt={name}
-        onClick={onRegistration}
-        className="event-image"
-      />
-      <h1 className="events-head">{name}</h1>
+      <button type="button" className="event-button">
+        <img
+          src={imageUrl}
+          alt="event"
+          onClick={onRegistration}
+          className="event-image"
+        />
+      </button>
+      <p className="events-head">{name}</p>
       <p className="events-para">{location}</p>
     </li>
   )
